aaca01dd51993e81bdcecd6623a67d7cfa59a6c4
Thzzao <thiago.santos1160@gmail.com>
2023-09-11 01:00:12 -0300

    refactor: Criação da lógica de validação do email e senha

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.png';
 import './global.css'
+import { useState } from 'react';
 
 export function App() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const [mensage, setMensage] = useState("")
+
+  function Valida(email, password) {
+    if (email === "thiago.santos1160@gmail.com" && password == 1234567) {
+      setMensage("Acessado com sucesso!")
+    } else {
+      setMensage("Usuário ou senha incorretos!")
+    }
+    setTimeout(() => {
+      setMensage("")
+    }, 5000);
+  }
+
   return (
     <div className="container">
       <header className="logo">
@@ -15,28 +33,38 @@ export function App() {
             type="text"
             name="email"
             id="email"
-            placeholder="Digite seu email">
+            placeholder="Digite seu email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}>
           </input>
         </div>
 
         <div className="inputContainer">
           <label htmlFor="senha">Senha</label>
           <input
-            type="senha"
+            type="password"
             name="senha"
             id="senha"
-            placeholder="Digite sua senha">
+            placeholder="Digite sua senha"
+            value={password}
+            onChange={e => setPassword(e.target.value)}>
           </input>
         </div>
 
-        <button className="botaoEntrar">Entrar</button>
+        <button onClick={(e) => {
+          e.preventDefault()
+          Valida(email, password)
+        }} className="botaoEntrar">Entrar</button>
 
         <div className="footer">
-          <label htmlFor="resultado"></label>
+          <label>{mensage}</label>
         </div>
+
       </form>
-
     </div>
-
   );
 }
+
+
+// Criar uma lógica para que a tela não suba e desça quando aparece a mensagem na label
+
