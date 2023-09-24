2c0431212e7763fb759fd35fae66330c3c81357f
Thzzao <thiago.santos1160@gmail.com>
2023-09-24 02:28:33 -0300

    feat: Criação da página home

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import '../style/global.css';
+
+export function Home() {
+
+    return (
+        <div className="infoUsuario">
+            {/* RETRONAR O NOME SOBRENOME E DATA DE NASCIEMNTO NA TELA  */}
+        </div>
+    )
+
+}
