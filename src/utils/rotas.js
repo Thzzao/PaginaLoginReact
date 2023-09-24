0e32c82d1b3a006cd071e22bdd6421b121c6776a
Thzzao <thiago.santos1160@gmail.com>
2023-09-24 02:30:20 -0300

    feat: Criação das rotas para acessar diferetes páginas

---
+++ src/utils/rotas.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import { Home } from '../pages/Home';
+import Cadastro from '../pages/Cadastro';
+import { Login } from '../pages/Login';
+
+const Rotas = () => {
+    return (
+        <BrowserRouter>
+            <Route exact={true} path="/" Component={Login} />
+            <Route exact={true} path="/cadastro" Component={Cadastro} />
+            <Route exact={true} path="/home" Component={Home} />
+        </BrowserRouter>
+    )
+}
+
+export default Rotas
