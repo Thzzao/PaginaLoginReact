6525d143e30bfd2cdff873cd519b06c558935632
Thzzao <thiago.santos1160@gmail.com>
2023-09-26 01:01:46 -0300

    fix: Alteração de configuração do firebase

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -4,13 +4,13 @@ import 'firebase/auth';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD66bjVlJm-CF31KfDEAquDNccaUxuoesw",
-    authDomain: "primeiro-projeto-em-react.firebaseapp.com",
-    projectId: "primeiro-projeto-em-react",
-    storageBucket: "primeiro-projeto-em-react.appspot.com",
-    messagingSenderId: "1087378395030",
-    appId: "1:1087378395030:web:5f67c537f1cda9db6457b7",
-    measurementId: "G-Z0BZL2MYEP"
+    apiKey: "AIzaSyCJBhgnfoZluKluarlFIBez2Jw5mVyFf3s",
+    authDomain: "site-react-3aef8.firebaseapp.com",
+    projectId: "site-react-3aef8",
+    storageBucket: "site-react-3aef8.appspot.com",
+    messagingSenderId: "994528400573",
+    appId: "1:994528400573:web:42569e2d81ec400e69d3d4",
+    measurementId: "G-NV2HXC3TW6"
 };
 
 if (!firebase.apps.length) {
