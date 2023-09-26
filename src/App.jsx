4a23b3fb747b96af0407abb9d5cd6c5301bb3363
Thzzao <thiago.santos1160@gmail.com>
2023-09-25 21:58:07 -0300

    feat: Criação do arquivo App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Rotas from './utils/rotas';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+export function App() {
+    return (
+        <Router>
+            <Rotas />
+        </Router>
+    );
+
+
+}
+
